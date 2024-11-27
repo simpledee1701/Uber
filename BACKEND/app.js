4dcc2b30cc3d64407b8bd79336cd4e71e14be6be
simpledee1701 <deepakvenugopalaiah2004@gmail.com>
2024-11-27 23:08:37 +0530

    Project setup push

---
+++ BACKEND/app.js
@@ -0,0 +1,15 @@
+const dotenv = this.require('dotenv');
+dotenv.config();
+
+const express =require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+
+app.get('/',(req,res) => {
+    res.send("Hello");
+})
+
+module.exports = app;
