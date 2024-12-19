429c092f431c10f04fbc1626b1dea340e4a4381e
simpledee1701 <deepakvenugopalaiah2004@gmail.com>
2024-12-20 00:27:22 +0530

    some chanes

--- FRONTEND/src/pages/CaptainHome.jsx
+++ FRONTEND/src/pages/CaptainHome.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import CaptainDetails from '../components/CAptainDetails'
+import CaptainDetails from '../components/CaptainDetails'
 import RidePopUp from '../components/RidePopUp'
 import { useGSAP } from '@gsap/react'
 import { gsap } from 'gsap';
